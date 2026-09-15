9fab564fe5fb177502faaf571b194b7b7331590f
Fabio Dias <137723212+fabiodias-eng@users.noreply.github.com>
2026-09-14 23:14:02 -0300

    chore: update dwmblocks display layout
    - remove trailing space between blocks underline if a block is abscent
    - remove battery block when message returns empty
    - update refresh rate

--- blocks.h
+++ blocks.h
@@ -1,18 +1,19 @@
 #define SchemeNorm 0
-#define SchemeSel  1
+#define SchemeSel 1
 
-
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sb-cpu", 1, 0},
-	{"", "sb-gpu", 1, 0},
-	{"", "sb-mem", 1, 0},
-	{"", "sb-disk", 60, 0},
-	{"", "sb-bat", 1, 0},
-	{"", "sb-wtr", 1800, 0},
-	{"", "sb-clock", 60, 0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", "sb-cpu", 1, 0},
+    {"", "sb-gpu", 1, 0},
+    {"", "sb-mem", 1, 0},
+    {"", "sb-disk", 60, 0},
+    {"", "sb-bat", 30, 0},
+    {"", "sb-wtr", 60, 0},
+    {"", "sb-clock", 60, 0},
 };
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
 static char delim[] = "|";
 static unsigned int delimLen = 0;
